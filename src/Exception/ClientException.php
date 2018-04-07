257d67e4aa8d23f7a0e27d3a56e2506150247dcf
Frank Koornstra <frank.koornstra@usabilla.com>
2018-04-07 17:19:55 +0200

    Add client interfaces
    For both a synchronous and asynchronous client

---
+++ src/Exception/ClientException.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace TechDeCo\ElasticApmAgent\Exception;
+
+use RuntimeException;
+
+final class ClientException extends RuntimeException implements Exception
+{
+}
---
+++ src/Exception/Exception.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace TechDeCo\ElasticApmAgent\Exception;
+
+/**
+ * An interface that tags all exceptions thrown by this library
+ */
+interface Exception
+{
+}
