64f7abdee654e4e968074a1469b7c3d848b99b1c
Frank Koornstra <frank@frankkoornstra.nl>
2018-04-14 19:15:31 +0200

    Add HTTPlug client wrapper
    That will add a span to the open transaction for each request it does.
    The open transaction needs to be injected before doing any requests. An
    extra header with a correlation id will be added to the request to be
    able to correlate cascading http calls.

---
+++ tests/Unit/Convenience/HttplugHttpClient/HttpClientDummy.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace TechDeCo\ElasticApmAgent\Tests\Unit\Convenience\HttplugHttpClient;
+
+use GuzzleHttp\Psr7\Response;
+use Http\Client\Exception;
+use Http\Client\Exception\NetworkException;
+use Http\Client\HttpClient;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use function usleep;
+
+final class HttpClientDummy implements HttpClient
+{
+    /**
+     * @var int
+     */
+    private $busyTime;
+
+    /**
+     * @var bool
+     */
+    private $throwsException;
+
+    /**
+     * @var RequestInterface
+     */
+    public $request;
+
+    public function __construct(int $busyTime, ?bool $throwsException = false)
+    {
+        $this->busyTime        = $busyTime;
+        $this->throwsException = $throwsException;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        $this->request = $request;
+
+        if ($this->throwsException) {
+            throw new NetworkException('foo', $request);
+        }
+
+        usleep($this->busyTime * 1000);
+
+        return new Response();
+    }
+}
