1c1429ebf171c0e73ca0985cdebc5894257d1140
Frank Koornstra <frank@frankkoornstra.nl>
2018-04-14 17:16:42 +0200

    Add PSR6 cache item pool wrapper
    That adds a span for every call made to the wrapped cache item pool. The
    open transaction needs to be injected before calling any methods of the
    cachen item pool interface.

---
+++ src/Convenience/Cache/CacheItemPoolWrapper.php
@@ -0,0 +1,224 @@
+<?php
+declare(strict_types=1);
+
+namespace TechDeCo\ElasticApmAgent\Convenience\Cache;
+
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Cache\InvalidArgumentException;
+use TechDeCo\ElasticApmAgent\Convenience\OpenTransaction;
+use TechDeCo\ElasticApmAgent\Convenience\OpenTransactionEnricher;
+use TechDeCo\ElasticApmAgent\Message\Span;
+use Traversable;
+use function implode;
+use function microtime;
+
+final class CacheItemPoolWrapper implements CacheItemPoolInterface, OpenTransactionEnricher
+{
+    /**
+     * @var CacheItemPoolInterface
+     */
+    private $pool;
+
+    /**
+     * @var OpenTransaction
+     */
+    private $transaction;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @param CacheItemPoolInterface $pool The pool that will be wrapped
+     * @param string                 $type The type of cache, for example redis, that will be reported in the span
+     */
+    public function __construct(
+        CacheItemPoolInterface $pool,
+        string $type
+    ) {
+        $this->pool = $pool;
+        $this->type = $type;
+    }
+
+    public function setOpenTransaction(OpenTransaction $transaction): void
+    {
+        $this->transaction = $transaction;
+    }
+
+    private function startTimer(): float
+    {
+        return microtime(true) * 1000;
+    }
+
+    private function stopTimer(float $start): float
+    {
+        return (microtime(true) * 1000) - $start;
+    }
+
+    /**
+     * @param string $key
+     * @throws InvalidArgumentException
+     */
+    public function getItem($key): CacheItemInterface
+    {
+        try {
+            $start = $this->startTimer();
+
+            return $this->pool->getItem($key);
+        } finally {
+            $this->transaction->addSpan(new Span(
+                $this->stopTimer($start),
+                $key,
+                $this->transaction->getStartOffset(),
+                $this->type . '.get-item'
+            ));
+        }
+    }
+
+    /**
+     * @param string[] $keys
+     * @throws InvalidArgumentException
+     * @return array|Traversable|CacheItemInterface[]
+     */
+    public function getItems(array $keys = [])
+    {
+        try {
+            $start = $this->startTimer();
+
+            return $this->pool->getItems($keys);
+        } finally {
+            $this->transaction->addSpan(new Span(
+                $this->stopTimer($start),
+                implode(',', $keys),
+                $this->transaction->getStartOffset(),
+                $this->type . '.get-items'
+            ));
+        }
+    }
+
+    /**
+     * @param string $key
+     * @throws InvalidArgumentException
+     */
+    public function hasItem($key): ?bool
+    {
+        try {
+            $start = $this->startTimer();
+
+            return $this->pool->hasItem($key);
+        } finally {
+            $this->transaction->addSpan(new Span(
+                $this->stopTimer($start),
+                $key,
+                $this->transaction->getStartOffset(),
+                $this->type . '.has-item'
+            ));
+        }
+    }
+
+    public function clear(): ?bool
+    {
+        try {
+            $start = $this->startTimer();
+
+            return $this->pool->clear();
+        } finally {
+            $this->transaction->addSpan(new Span(
+                $this->stopTimer($start),
+                '',
+                $this->transaction->getStartOffset(),
+                $this->type . '.clear'
+            ));
+        }
+    }
+
+    /**
+     * @param string $key
+     * @throws InvalidArgumentException
+     */
+    public function deleteItem($key): ?bool
+    {
+        try {
+            $start = $this->startTimer();
+
+            return $this->pool->deleteItem($key);
+        } finally {
+            $this->transaction->addSpan(new Span(
+                $this->stopTimer($start),
+                $key,
+                $this->transaction->getStartOffset(),
+                $this->type . '.delete-item'
+            ));
+        }
+    }
+
+    /**
+     * @param string[] $keys
+     * @throws InvalidArgumentException
+     */
+    public function deleteItems(array $keys): ?bool
+    {
+        try {
+            $start = $this->startTimer();
+
+            return $this->pool->deleteItems($keys);
+        } finally {
+            $this->transaction->addSpan(new Span(
+                $this->stopTimer($start),
+                implode(',', $keys),
+                $this->transaction->getStartOffset(),
+                $this->type . '.delete-items'
+            ));
+        }
+    }
+
+    public function save(CacheItemInterface $item): ?bool
+    {
+        try {
+            $start = $this->startTimer();
+
+            return $this->pool->save($item);
+        } finally {
+            $this->transaction->addSpan(new Span(
+                $this->stopTimer($start),
+                $item->getKey(),
+                $this->transaction->getStartOffset(),
+                $this->type . '.save'
+            ));
+        }
+    }
+
+    public function saveDeferred(CacheItemInterface $item): ?bool
+    {
+        try {
+            $start = $this->startTimer();
+
+            return $this->pool->saveDeferred($item);
+        } finally {
+            $this->transaction->addSpan(new Span(
+                $this->stopTimer($start),
+                $item->getKey(),
+                $this->transaction->getStartOffset(),
+                $this->type . '.save-deferred'
+            ));
+        }
+    }
+
+    public function commit(): ?bool
+    {
+        try {
+            $start = $this->startTimer();
+
+            return $this->pool->commit();
+        } finally {
+            $this->transaction->addSpan(new Span(
+                $this->stopTimer($start),
+                'commit',
+                $this->transaction->getStartOffset(),
+                $this->type . '.commit'
+            ));
+        }
+    }
+}
---
+++ tests/Unit/Convenience/Cache/DummyCacheItemPool.php
@@ -0,0 +1,147 @@
+<?php
+declare(strict_types=1);
+
+namespace TechDeCo\ElasticApmAgent\Tests\Unit\Convenience\Cache;
+
+use Cache\Adapter\Common\Exception\InvalidArgumentException as InvalidArgumentExceptionImpl;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Cache\InvalidArgumentException;
+use function usleep;
+
+final class DummyCacheItemPool implements CacheItemPoolInterface
+{
+    /**
+     * @var CacheItemInterface
+     */
+    private $item;
+
+    /**
+     * @var int
+     */
+    private $busyTime;
+
+    /**
+     * @var bool
+     */
+    private $throwsException;
+
+    public function __construct(CacheItemInterface $item, int $busyTime, ?bool $throwsException = false)
+    {
+        $this->item            = $item;
+        $this->busyTime        = $busyTime;
+        $this->throwsException = $throwsException;
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    private function createResponse(): CacheItemInterface
+    {
+        if ($this->throwsException) {
+            throw new InvalidArgumentExceptionImpl('test');
+        }
+
+        usleep($this->busyTime * 1000);
+
+        return $this->item;
+    }
+
+    /**
+     * @param string $key
+     * @throws InvalidArgumentException
+     * @return CacheItemInterface
+     */
+    public function getItem($key)
+    {
+        return $this->createResponse();
+    }
+
+    /**
+     * @param string[] $keys
+     * @throws InvalidArgumentException
+     * @return array|\Traversable
+     */
+    public function getItems(array $keys = [])
+    {
+        return [
+            $this->createResponse(),
+        ];
+    }
+
+    /**
+     * @param string $key
+     * @throws InvalidArgumentException
+     * @return bool
+     */
+    public function hasItem($key)
+    {
+        $this->createResponse();
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function clear()
+    {
+        usleep($this->busyTime * 1000);
+
+        return true;
+    }
+
+    /**
+     * @param string $key
+     * @throws InvalidArgumentException
+     * @return bool
+     */
+    public function deleteItem($key)
+    {
+        $this->createResponse();
+
+        return true;
+    }
+
+    /**
+     * @param string[] $keys
+     * @throws InvalidArgumentException
+     * @return bool
+     */
+    public function deleteItems(array $keys)
+    {
+        $this->createResponse();
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function save(CacheItemInterface $item)
+    {
+        usleep($this->busyTime * 1000);
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function saveDeferred(CacheItemInterface $item)
+    {
+        usleep($this->busyTime * 1000);
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function commit()
+    {
+        usleep($this->busyTime * 1000);
+
+        return true;
+    }
+}
