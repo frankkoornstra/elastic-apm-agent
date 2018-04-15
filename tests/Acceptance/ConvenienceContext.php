e607e308f92ae099959c5edb2a8ad83fe7aff61b
Frank Koornstra <frank@frankkoornstra.nl>
2018-04-15 10:56:46 +0200

    Add acceptance test for http client

--- tests/Acceptance/ConvenienceContext.php
+++ tests/Acceptance/ConvenienceContext.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 namespace TechDeCo\ElasticApmAgent\Tests\Acceptance;
 
 use Behat\Behat\Context\Context;
+use GuzzleHttp\Psr7\Request;
+use Http\Client\HttpClient;
 use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\AssertionFailedError;
 use PHPUnit\Framework\ExpectationFailedException;
 use Psr\Cache\CacheItemPoolInterface;
 use Ramsey\Uuid\Uuid;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
 use TechDeCo\ElasticApmAgent\Convenience\OpenTransaction;
 use TechDeCo\ElasticApmAgent\Convenience\OpenTransactionEnricher;
 use TechDeCo\ElasticApmAgent\Message\Timestamp;
@@ -20,6 +24,11 @@ final class ConvenienceContext implements Context
     private $cache;
 
     /**
+     * @var HttpClient|OpenTransactionEnricher
+     */
+    private $httpClient;
+
+    /**
      * @var OpenTransaction|null
      */
     private $open;
@@ -29,9 +38,10 @@ final class ConvenienceContext implements Context
      */
     private $closed;
 
-    public function __construct(CacheItemPoolInterface $cache)
+    public function __construct(CacheItemPoolInterface $cache, HttpClient $httpClient)
     {
-        $this->cache = $cache;
+        $this->cache      = $cache;
+        $this->httpClient = $httpClient;
     }
 
     /**
@@ -47,14 +57,7 @@ final class ConvenienceContext implements Context
         );
 
         $this->cache->setOpenTransaction($this->open);
-    }
-
-    /**
-     * @Given I get item :key from cache
-     */
-    public function iGetItemFromCache(string $key): void
-    {
-        $this->cache->getItem($key);
+        $this->httpClient->setOpenTransaction($this->open);
     }
 
     /**
@@ -66,20 +69,54 @@ final class ConvenienceContext implements Context
     }
 
     /**
-     * @Then the closed transaction has a cache span for getting item :key
+     * @throws AssertionFailedError
+     * @throws InvalidArgumentException
      */
-    public function theClosedTransactionHasACacheSpanForGettingItem(string $key): void
+    private function assertSpanExists(string $key, string $type): void
     {
         foreach ($this->closed['spans'] as $span) {
             try {
                 Assert::assertSame($key, $span['name']);
-                Assert::assertStringEndsWith('.get-item', $span['type']);
+                Assert::assertSame($type, $span['type']);
 
                 return;
             } catch (ExpectationFailedException $e) {
             }
         }
 
-        Assert::fail('No matching span found');
+        Assert::fail('No matching span exists');
+    }
+
+    /**
+     * @Given I get item :key from cache
+     */
+    public function iGetItemFromCache(string $key): void
+    {
+        $this->cache->getItem($key);
+    }
+
+    /**
+     * @Then the closed transaction has a cache span for getting item :key
+     */
+    public function theClosedTransactionHasACacheSpanForGettingItem(string $key): void
+    {
+        $this->assertSpanExists($key, 'dummy.get-item');
+    }
+
+    /**
+     * @Given I send an http request for :url
+     */
+    public function iSendAnHttpRequestFor(string $url): void
+    {
+        $request = new Request('GET', $url);
+        $this->httpClient->sendRequest($request);
+    }
+
+    /**
+     * @Then the closed transaction has an http request span for :url
+     */
+    public function theClosedTransactionHasAnHttpRequestSpanFor(string $url): void
+    {
+        $this->assertSpanExists('GET ' . $url, 'http.request');
     }
 }
