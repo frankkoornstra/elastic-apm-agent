ddb4d44898e282ed3809a82ef9503f39d1918e36
Frank Koornstra <frank@frankkoornstra.nl>
2018-05-07 07:33:39 +0200

    Add missing message tests

--- tests/Unit/Message/ContextTest.php
+++ tests/Unit/Message/ContextTest.php
@@ -5,6 +5,8 @@ namespace TechDeCo\ElasticApmAgent\Tests\Unit\Message;
 
 use PHPUnit\Framework\TestCase;
 use TechDeCo\ElasticApmAgent\Message\Context;
+use TechDeCo\ElasticApmAgent\Message\Response;
+use TechDeCo\ElasticApmAgent\Message\User;
 
 final class ContextTest extends TestCase
 {
@@ -12,11 +14,15 @@ final class ContextTest extends TestCase
     {
         $actual = (new Context())
             ->withCustomVariable('a', 'behemoth')
+            ->withResponse((new Response())->resultingInStatusCode(200))
             ->withTag('b', 'charger')
+            ->withUser((new User())->withId(9))
             ->jsonSerialize();
 
         $expected = [
             'custom' => ['a' => 'behemoth'],
+            'response' => ['status_code' => 200],
+            'user' => ['id' => 9],
             'tags' => ['b' => 'charger'],
         ];
 
