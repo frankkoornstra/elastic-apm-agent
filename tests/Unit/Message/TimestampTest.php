77a072358443049babcdcc6d5b48c5f82a75e56c
Mikol Faro <mikol.faro@gmail.com>
2018-08-17 12:43:34 +0200

    Fix broken circleci

--- tests/Unit/Message/TimestampTest.php
+++ tests/Unit/Message/TimestampTest.php
@@ -14,14 +14,14 @@ final class TimestampTest extends TestCase
      */
     private $oldTimeZone;
 
-    public function setUp()
+    public function setUp(): void
     {
-        $this->oldTimeZone = date_default_timezone_get();
+        $this->oldTimeZone = \date_default_timezone_get();
     }
 
     public function testWithUtc(): void
     {
-        date_default_timezone_set('UTC');
+        \date_default_timezone_set('UTC');
         $date = new Timestamp('2018-01-15 12:00');
 
         self::assertEquals('2018-01-15T12:00:00.000000Z', $date->jsonSerialize());
@@ -29,14 +29,14 @@ final class TimestampTest extends TestCase
 
     public function testWithOtherTimezone(): void
     {
-        date_default_timezone_set('Asia/Tokyo');
+        \date_default_timezone_set('Asia/Tokyo');
         $date = new Timestamp('2018-01-15 12:00');
 
         self::assertEquals('2018-01-15T03:00:00.000000Z', $date->jsonSerialize());
     }
 
-    public function tearDown()
+    public function tearDown(): void
     {
-        date_default_timezone_set($this->oldTimeZone);
+        \date_default_timezone_set($this->oldTimeZone);
     }
 }
